8a6df4a2600d6c7d51c09767b8005f78257a77a1
G-Aathish <gaaathish@gmail.com>
2026-07-22 14:47:21 +0530

    Time: 2 ms (34.82%) | Memory: 45.6 MB (46.24%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,41 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
 class Solution {
-    int stkSize = 0;
-    
-    
-    public void getPermutations(Stack<Integer> stk, boolean[] used, int[] nums, List<List<Integer>> permutations) {
-        if (nums.length == this.stkSize) {
-            permutations.add(new ArrayList<>(stk));
-            return;
-        }
-        
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i] == true)
-                continue;
-            
-            
-            stk.push(nums[i]);
-            used[i] = true;
-            this.stkSize++;
-            
-            
-            getPermutations(stk, used, nums, permutations);
-            
-            
-            stk.pop();
-            used[i] = false;
-            this.stkSize--;
-        }
-    }
-    
-    
+    // Tracking the size manually avoids overhead from repeated container calls
+    private int stkSize = 0;
+
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> permutations = new ArrayList<>();
         boolean[] used = new boolean[nums.length];
         Stack<Integer> stk = new Stack<>();
         
-        
         getPermutations(stk, used, nums, permutations);
         return permutations;
+    }
+
+    private void getPermutations(Stack<Integer> stk, boolean[] used, int[] nums, List<List<Integer>> permutations) {
+        // Base case: we have selected elements equal to the total length
+        if (nums.length == this.stkSize) {
+            permutations.add(new ArrayList<>(stk));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            // Choose element
+            stk.push(nums[i]);
+            used[i] = true;
+            this.stkSize++;
+
+            // Explore paths recursively
+            getPermutations(stk, used, nums, permutations);
+
+            // Unchoose element (Backtrack)
+            stk.pop();
+            used[i] = false;
+            this.stkSize--;
+        }
     }
 }
